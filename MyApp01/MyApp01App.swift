22ad45a14e86059c17c0169cd40b3a04fd32ce68
dotadang <dotadang@126.com>
2026-03-31 17:48:42 +0800

    Initial Commit

---
+++ MyApp01/MyApp01App.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp01App.swift
+//  MyApp01
+//
+//  Created by Qiang on 2026/3/31.
+//
+
+import SwiftUI
+
+@main
+struct MyApp01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
